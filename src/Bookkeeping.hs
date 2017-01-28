0425e961a96d6e900423871841328bb0852fa751
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-01-28 23:49:36 +0900

    Create basic data types.

---
+++ src/Bookkeeping.hs
@@ -0,0 +1,32 @@
+module Bookkeeping
+  (
+  ) where
+
+import Control.Monad.State (State)
+
+newtype Transactions m a =
+  Transactions { unTransactions :: StateT Transaction m a }
+
+data Transaction = Transaction
+  { tYear        :: {-# UNPACK #-} !Int
+  , tMonth       :: {-# UNPACK #-} !Int
+  , tDate        :: {-# UNPACK #-} !Int
+  , tDescription :: {-# UNPACK #-} !Text
+  , tSubDesc     :: {-# UNPACK #-} !Text
+  , tCategory    :: {-# UNPACK #-} !Text
+  , tAmount      :: {-# UNPACK #-} !Int
+  } deriving (Show, Read, Ord, Eq, Enum)
+
+
+data Category = Category
+  { name :: {-# UNPACK #-} !Text
+  , categoryType :: {-# UNPACK #-} !CategoryType
+  } deriving (Show, Read, Ord, Eq, Enum)
+
+
+data CategoryType
+  = Assets
+  | Liabilities
+  | Stock
+  | Revenue
+  | Expenses
