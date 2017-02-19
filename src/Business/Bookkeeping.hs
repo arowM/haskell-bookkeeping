579bfafab97e0623d968eea5391487fde5e1f73a
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-02-19 23:08:14 +0900

    Refactoring for publishing to hackage.

--- src/Business/Bookkeeping.hs
+++ src/Business/Bookkeeping.hs
@@ -1,22 +1,59 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+{- |
+Module      :  Business.Bookkeeping
+
+Copyright   :  Kadzuya Okamoto 2017
+License     :  MIT
+
+Stability   :  experimental
+Portability :  unknown
+
+This module exports core functions and types for bookkeeping.
+-}
 module Business.Bookkeeping
-  ( year
+  (
+  -- * Usage examples
+  -- $setup
+  -- * Constructors
+    year
   , month
   , activity
   , dateTrans
+
+  -- * Converters
   , runTransactions
+
+  -- * Pritty printers
   , ppr
-  , module X
+
+  -- * Types
+  , Transactions
+  , YearTransactions
+  , MonthTransactions
+  , DateTransactions
+  , Transaction(..)
+  , Year(..)
+  , Month(..)
+  , Date(..)
+  , Description(..)
+  , Amount(..)
+  , Category(..)
+  , CategoryName(..)
+  , CategoryType(..)
+  , DebitCategory(..)
+  , CreditCategory(..)
   ) where
 
-import Control.Monad.State (execState, modify)
+import Control.Monad.State (State, execState, modify)
 import qualified Data.DList as DList
+import Data.DList (DList)
 import Data.Monoid ((<>))
+import Data.String (IsString(..))
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Time.Calendar (fromGregorian)
-
-import Business.Bookkeeping.Types
-import qualified Business.Bookkeeping.Types as X
+import Data.Time.Calendar (Day, fromGregorian)
 
 {- $setup
 >>> :{
@@ -40,23 +77,20 @@ let
 {-| Convert from 'YearTransactions' to 'Transactions'.
 -}
 year :: Year -> YearTransactions -> Transactions
-year y yts = modify . flip mappend $ fmap ($ y) fs
-  where
-    fs = execState yts mempty
+year y =
+  modify . flip mappend . fmap ($ y) . toDList
 
 {-| Convert from 'MonthTransactions' to 'YearTransactions'.
 -}
 month :: Month -> MonthTransactions -> YearTransactions
-month m mts = modify . flip mappend $ fmap ($ m) fs
-  where
-    fs = execState mts mempty
+month m =
+  modify . flip mappend . fmap ($ m) . toDList
 
 {-| Convert from 'DateTransactions' to 'MonthTransactions'.
 -}
 activity :: Date -> Description -> DateTransactions -> MonthTransactions
-activity d desc dts = modify . flip mappend $ fmap (($ desc) . ($ d)) fs
-  where
-    fs = execState dts mempty
+activity d desc =
+  modify . flip mappend . fmap (($ desc) . ($ d)) . toDList
 
 dateTrans :: DebitCategory
           -> CreditCategory
@@ -64,9 +98,7 @@ dateTrans :: DebitCategory
           -> Amount
           -> DateTransactions
 dateTrans debit credit desc amount =
-  modify $
-  flip mappend $
-  DList.singleton $ \d desc' m y ->
+  modify . flip mappend . DList.singleton $ \d desc' m y ->
     Transaction
     { tDay = fromGregorian (unYear y) (unMonth m) (unDate d)
     , tDescription = desc' <> " " <> desc
@@ -75,10 +107,16 @@ dateTrans debit credit desc amount =
     , tAmount = amount
     }
 
+{-| Take list of `Transaction` out from 'Transactions'.
+-}
 runTransactions :: Transactions -> [Transaction]
-runTransactions ts = DList.toList $ execState ts mempty
+runTransactions = DList.toList . toDList
+
+toDList :: Trans a -> DList a
+toDList ts = execState ts mempty
 
 {-| A pretty printer for `Transactions`.
+
 >>> ppr sample
 tDay: 2015-01-03
 tDescription: 予定1 最初の預金
@@ -123,3 +161,87 @@ ppr = T.putStr . T.unlines . map format . runTransactions
           ")"
         , "tAmount: " <> (T.pack . show . unAmount) tAmount
         ]
+
+
+{- ==============
+ -     Types
+ - ============== -}
+
+type Trans a = State (DList a) ()
+
+{-| A type for handling `Transaction` values.
+ -}
+type Transactions = Trans Transaction
+
+type YearTransactions = Trans (Year -> Transaction)
+
+type MonthTransactions = Trans (Month -> Year -> Transaction)
+
+type DateTransactions = Trans (Date -> Description -> Month -> Year -> Transaction)
+
+{-| A type representing a transaction.
+ -}
+data Transaction = Transaction
+  { tDay :: Day
+  , tDescription :: Description
+  , tDebit :: DebitCategory
+  , tCredit :: CreditCategory
+  , tAmount :: Amount
+  } deriving (Show, Read, Ord, Eq)
+
+newtype Year = Year
+  { unYear :: Integer
+  } deriving (Show, Read, Ord, Eq, Num)
+
+newtype Month = Month
+  { unMonth :: Int
+  } deriving (Show, Read, Ord, Eq, Num)
+
+newtype Date = Date
+  { unDate :: Int
+  } deriving (Show, Read, Ord, Eq, Num)
+
+newtype Description = Description
+  { unDescription :: Text
+  } deriving (Show, Read, Ord, Eq)
+
+instance IsString Description where
+  fromString = Description . fromString
+
+instance Monoid Description where
+  mempty = Description mempty
+  mappend (Description a) (Description b) = Description $ mappend a b
+
+newtype Amount = Amount
+  { unAmount :: Int
+  } deriving (Show, Read, Ord, Eq, Num)
+
+newtype DebitCategory = DebitCategory
+  { unDebitCategory :: Category
+  } deriving (Show, Read, Ord, Eq)
+
+newtype CreditCategory = CreditCategory
+  { unCreditCategory :: Category
+  } deriving (Show, Read, Ord, Eq)
+
+{-| A type representing an accounts title.
+ -}
+data Category = Category
+  { cName :: CategoryName
+  , cType :: CategoryType
+  } deriving (Show, Read, Ord, Eq)
+
+newtype CategoryName = CategoryName
+  { unCategoryName :: Text
+  } deriving (Show, Read, Ord, Eq)
+
+instance IsString CategoryName where
+  fromString = CategoryName . fromString
+
+data CategoryType
+  = Assets
+  | Liabilities
+  | Stock
+  | Revenue
+  | Expenses
+  deriving (Show, Read, Ord, Eq, Enum)
